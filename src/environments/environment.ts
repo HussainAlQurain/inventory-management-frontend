69d80c08bc09a630d430cedec2cd7d4ef1976c4c
Hussain <g202316090@kfupm.edu.sa>
2025-05-10 13:11:06 +0300

    increased the budget size for app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://localhost:8080'
+  production: true,
+  apiUrl: 'http://localhost:8080',
+  posApiUrl: 'http://localhost:8888'
 };
